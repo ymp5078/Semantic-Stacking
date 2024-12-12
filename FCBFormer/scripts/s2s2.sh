39f12325148fa467ce3fa6bb1631c0ba24702758
ymp5078 <ymp5078@psu.edu>
2024-12-12 14:57:26 -0600

    clean .sh

--- FCBFormer/scripts/s2s2.sh
+++ FCBFormer/scripts/s2s2.sh
@@ -3,15 +3,15 @@ CVC_PATH=[cvc_dataset_path]
 KVASIR_PATH=[kvasir_dataset_path]
 SAVEPATH=[s2s2_result_path]
 
-python -W ignore train.py --dataset=CVC_gen --data-root=/scratch/bbmr/ymp5078/segmentations/data/Polyp_data/CVC-ClinicDB/ --gen-input-path /scratch/bbmr/ymp5078/segmentations/ControlNet/CVC_gen_16 --consist_loss_weight 0.4 --kd_loss_weight 0.4 --gen_image --multi-gpu true --save-path $SAVEPATH
-python -W ignore train.py --dataset=Kvasir_gen --data-root=/scratch/bbmr/ymp5078/segmentations/data/Polyp_data/Kvasir-SEG/ --gen-input-path /scratch/bbmr/ymp5078/segmentations/ControlNet/Kvasir_gen_16 --consist_loss_weight 0.4 --kd_loss_weight 0.4 --gen_image --multi-gpu true --save-path $SAVEPATH
+python -W ignore train.py --dataset=CVC_gen --data-root=$CVC_PATH --gen-input-path [gen_output_path] --consist_loss_weight 0.4 --kd_loss_weight 0. --gen_image --multi-gpu true --save-path $SAVEPATH
+python -W ignore train.py --dataset=Kvasir_gen --data-root=$KVASIR_PATH --gen-input-path [gen_output_path] --consist_loss_weight 0.4 --kd_loss_weight 0. --gen_image --multi-gpu true --save-path $SAVEPATH
 
-python predict.py --train-dataset=CVC_gen --test-dataset=CVC --data-root=/scratch/bbmr/ymp5078/segmentations/data/Polyp_data/CVC-ClinicDB/ --save-path $SAVEPATH
-python predict.py --train-dataset=Kvasir_gen --test-dataset=CVC --data-root=/scratch/bbmr/ymp5078/segmentations/data/Polyp_data/CVC-ClinicDB/ --save-path $SAVEPATH
-python predict.py --train-dataset=CVC_gen --test-dataset=Kvasir --data-root=/scratch/bbmr/ymp5078/segmentations/data/Polyp_data/Kvasir-SEG/ --save-path $SAVEPATH
-python predict.py --train-dataset=Kvasir_gen --test-dataset=Kvasir --data-root=/scratch/bbmr/ymp5078/segmentations/data/Polyp_data/Kvasir-SEG/ --save-path $SAVEPATH
+python predict.py --train-dataset=CVC_gen --test-dataset=CVC --data-root=$CVC_PATH --save-path $SAVEPATH
+python predict.py --train-dataset=Kvasir_gen --test-dataset=CVC --data-root=$CVC_PATH --save-path $SAVEPATH
+python predict.py --train-dataset=CVC_gen --test-dataset=Kvasir --data-root=$KVASIR_PATH --save-path $SAVEPATH
+python predict.py --train-dataset=Kvasir_gen --test-dataset=Kvasir --data-root=$KVASIR_PATH --save-path $SAVEPATH
 
-python eval.py --train-dataset=CVC_gen --test-dataset=CVC --data-root=/scratch/bbmr/ymp5078/segmentations/data/Polyp_data/CVC-ClinicDB/ --save-path $SAVEPATH
-python eval.py --train-dataset=Kvasir_gen --test-dataset=CVC --data-root=/scratch/bbmr/ymp5078/segmentations/data/Polyp_data/CVC-ClinicDB/ --save-path $SAVEPATH
-python eval.py --train-dataset=CVC_gen --test-dataset=Kvasir --data-root=/scratch/bbmr/ymp5078/segmentations/data/Polyp_data/Kvasir-SEG/ --save-path $SAVEPATH
-python eval.py --train-dataset=Kvasir_gen --test-dataset=Kvasir --data-root=/scratch/bbmr/ymp5078/segmentations/data/Polyp_data/Kvasir-SEG/ --save-path $SAVEPATH
+python eval.py --train-dataset=CVC_gen --test-dataset=CVC --data-root=$CVC_PATH --save-path $SAVEPATH
+python eval.py --train-dataset=Kvasir_gen --test-dataset=CVC --data-root=$CVC_PATH --save-path $SAVEPATH
+python eval.py --train-dataset=CVC_gen --test-dataset=Kvasir --data-root=$KVASIR_PATH --save-path $SAVEPATH
+python eval.py --train-dataset=Kvasir_gen --test-dataset=Kvasir --data-root=$KVASIR_PATH --save-path $SAVEPATH
